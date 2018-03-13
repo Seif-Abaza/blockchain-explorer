a8f301665226d7c574d33c96cd48d1c2a8e3b31e
rostog <rostogiorgi@gmail.com>
2018-03-13 10:44:14 +0100

    added vscode config

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-/* @flow */
+// @flow
 import * as React from 'react';
+import TransactionPage from './pages/Transactions/TransactionsPage';
 
 type Props = {
-  bar?: string,
+  /* ... */
 };
 
-class App extends React.Component<Props> {
+type State = {
+  count: number,
+};
+
+class App extends React.Component<Props, State> {
+  componentDidMount() {}
+
   render() {
-    return <div>{this.props.bar}</div>;
+    return <TransactionPage />;
   }
 }
 
